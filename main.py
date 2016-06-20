781279c049ef8a5e3a7c0fbabf8944fd94dc0c0d
Aleks Merser <enderman.com290@gmail.com>
2016-06-21 02:05:25 +0400

    Полностью переделан алгоритм
    Наканец та норм так команды
    Все работает
    Пока шо пустышка,нос следующий апдейт будет ух как заебись просто

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-print("This is console module")
-print("Программа «MoneyBall API»\nВерсия: 0.5")
+print("Программа «MoneyBall API»\nВерсия: 1.0")
 print("\nРазработчики:\n Xaker Su - mcg76\n Максим Удачин - Bruno99")
 print("\nНаш Веб-Сайт: http://vk.com/dev.corp.python\n")
-
-
+##Глобальная хуйня##
+global money
+money = 0
 ##База данных v.1##
 
 moneydb = {
@@ -16,57 +16,47 @@ userik = {
 vip = []
 elite = []
 admin = ["admin"]
-##Регистрация##
 
+##Регистрация##
 print("\nЧтобы начать работать с программой, зарегистрируйтесь в нашей системе:\n")
 user = str(input("Пользователь: "))
 passw = str(input("Пароль: "))
 saves = user,":",passw
 users.append(saves)
-money = 0
 snickmoney = moneydb.fromkeys([user], money)
 don = "Пользователь"
-if user == admin:
+if not user == admin:
  print("Загрузка....")
 else:
  print("Здраствуйте Администратор!")
 #if user in moneydb:
  #money = moneydb[user]
-print("\nРегистрация завершена, как: \n Пользователь - ",user,"\n Пароль - ",passw)
 if user in users:
  print("Wrong!Error 404")
 else:
- print("\nПроверка завершена, пользователь «",user,"» присутсвует в Базе Данных.")
-
-##Добавление денег##
-if not user in admin:
- print("доступ к добавлению денег запрещен!")
-else:
- don = "Администратор"
- print("Вы может изменить баланс!")
- try:
-  addmoney = int(input("\nДобавьте сумму на ваш баланс: "))
-  money += addmoney
- except ValueError:
-  print("Ошибка ввода, пожалуйста используйте цифры!")
-  print("\nПовторная попытка: ")
-  addmoney = int(input(" Добавьте сумму на ваш баланс: "))
-  kek = addmoney + money
-  moneydb[user] = kek
- ##Проверка Баланса##
-while True:
- moneys = str(input("\nВведите команду на проверку баланса(Не знаете команды?пропишите /help): "))
- if moneys == "/money":
-  print("\nПользователь «",user,"», Баланс: ",money,",Доступ: ",don,"\n\n")
- else:
-  print("Команда не совпадает!")
-  print("Повторите ввод команды:")
-  break
-  if not moneys == "/quit":
-   print("Exiting..")
-   raise ValueError
-  else:
+ print("\nРегистрация завершена, как: \n Пользователь - ",user,"\n Пароль - ",passw)
+ print("\nПроверка завершена, пользователь «",user,"» присуствует в Базе Данных.")
+##Команды нахуй##
+def helps():
+ print("Говно залупа")
+def addmoneys():
+ while True:
+  try:
+   addmoney = int(input("\nДобавьте сумму на ваш баланс: "))
+  except ValueError:
+   print("Ошибка ввода, пожалуйста используйте цифры!")
+   print("\nПовторная попытка: ")
    continue
- ##return сюда!!
-# Конец программы(Дно) #
-print("----------"*5,"\n     Работа программы завершена! ©DevCorp\n","----------"*5)
+  finally:
+   break
+   kek = addmoney + money
+   moneydb[user] = kek
+commands = {'/helps': helps,'/addmoney': addmoneys}
+while True:
+ comsend = input("Введите команду:")
+ command = comsend
+ if command in commands:
+  commands[comsend]()
+ else:
+  print("Хуйню ввел")
+  continue
