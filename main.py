bc27019c29be1dad9d736dc3dd4a65bccb831576
Aleks Merser <enderman.com290@gmail.com>
2016-06-22 11:45:35 +0400

    Ничего не сделано,нмчего не работает,фиксии

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 print("Программа «MoneyBall API»\nВерсия: 1.0")
 print("\nРазработчики:\n Xaker Su - mcg76\n Максим Удачин - Bruno99")
 print("\nНаш Веб-Сайт: http://vk.com/dev.corp.python\n")
-##Глобал##
+##Глобальная хуйня##
 global money
 money = 0
 ##База данных v.1##
-
 moneydb = {
     "nick": "money"
 }
-users = ["admin:229"]
+users = {"admin": 229}
 userik = {
 	   "nick": "привелегия"
 }
@@ -21,24 +20,35 @@ admin = ["admin"]
 print("\nЧтобы начать работать с программой, зарегистрируйтесь в нашей системе:\n")
 user = str(input("Пользователь: "))
 passw = str(input("Пароль: "))
-saves = user,":",passw
-users.append(saves)
-snickmoney = moneydb.fromkeys([user], money)
+if user in users:
+ print("Вы уже зарегистрированы")
+else:
+ users = dict(user = passw)
+ snickmoney = moneydb.fromkeys([user], money)
 don = "Пользователь"
-if not user == admin:
+if user == admin:
  print("Загрузка....")
 else:
  print("Здраствуйте Администратор!")
+ don = "Администратор"
 #if user in moneydb:
  #money = moneydb[user]
 if user in users:
- print("Wrong!Error 404")
+ print("Wrong!Error 404!Профиль уже создан или не найден")
 else:
  print("\nРегистрация завершена, как: \n Пользователь - ",user,"\n Пароль - ",passw)
  print("\nПроверка завершена, пользователь «",user,"» присуствует в Базе Данных.")
-##Команды##
+##Команды нахуй##
+def dbcheck():
+ print(moneydb)
+ print(users)
+def moneyss():
+ if not user == admin:
+  print(moneydb)
+ else:
+  print("В разработке")
 def helps():
- print("Commands to:")
+ print("Говно залупа")
 def addmoneys():
  while True:
   try:
@@ -51,12 +61,13 @@ def addmoneys():
    break
    kek = addmoney + money
    moneydb[user] = kek
-commands = {'/helps': helps,'/addmoney': addmoneys}
+   users[user] = passw
+commands = {'/helps': helps,'/addmoney': addmoneys,'/mymoney': moneyss,'/getbd': dbcheck}
 while True:
  comsend = input("Введите команду:")
  command = comsend
  if command in commands:
   commands[comsend]()
  else:
-  print("Ваша жизнь ошибка..")
+  print("Хуйню ввел")
   continue
