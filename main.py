7ac204e56c4c59c2fbc14c7db4034b3e0eebcf24
Aleks Merser <enderman.com290@gmail.com>
2016-06-22 14:13:33 +0400

    Глобальное обновление 2.0
    Добавленл:
    Регистрация + автологин
    Главное нововведение это логин система
    Режим анонима
    Добавление денег только со стороны Администратора
    Enjoy!
    Планируется:
    Доработать деньги 
    Магазин

--- main.py
+++ main.py
@@ -1,67 +1,87 @@
-print("Программа «MoneyBall API»\nВерсия: 1.0")
+print("Программа «MoneyBall API»\nВерсия: 2.0")
 print("\nРазработчики:\n Xaker Su - mcg76\n Максим Удачин - Bruno99")
 print("\nНаш Веб-Сайт: http://vk.com/dev.corp.python\n")
 ##Глобальная хуйня##
+import time
 global money
 money = 0
 ##База данных v.1##
-moneydb = {
-    "nick": "money"
-}
-users = {"admin": 229}
+moneydb = {'kok': 0}
+users = {'keker': 123,'admin': 229}
 userik = {
 	   "nick": "привелегия"
 }
 vip = []
 elite = []
 admin = ["admin"]
-
 ##Регистрация##
 print("\nЧтобы начать работать с программой, зарегистрируйтесь в нашей системе:\n")
 user = str(input("Пользователь: "))
-passw = str(input("Пароль: "))
+while True:
+ try:
+  passw = int(input("Пароль: "))
+  break
+ except ValueError:
+  print("Пожалуйста используйте цифры")
+  continue
 if user in users:
  print("Вы уже зарегистрированы")
+ print("Сейчас проверю тебя и введеные данные")
+ time.sleep(2)
+ if not users[user] == passw:
+   print("Ошибка!!!Неверные данные")
+   don = "Аноним"
+   print("Сейчас вы в режиме: ",don)
+ else:
+   print("Логин завершен как: ",user,)
+ #if key in users:
+   #else:
+  #print("Логин завершен как: ",user,)
 else:
- users = dict(user = passw)
- snickmoney = moneydb.fromkeys([user], money)
+ print("\nРегистрация завершена, как: \n Пользователь - ",user,"\n Пароль - ",passw)
+ ##users = dict(user, passw)
+ other = user,passw
+ users.update([other])
+ mymoney = user,0
+ moneydb.update([mymoney])
 don = "Пользователь"
-if user == admin:
+if user == "Администратор":
  print("Загрузка....")
 else:
  print("Здраствуйте Администратор!")
  don = "Администратор"
 #if user in moneydb:
  #money = moneydb[user]
-if user in users:
- print("Wrong!Error 404!Профиль уже создан или не найден")
-else:
- print("\nРегистрация завершена, как: \n Пользователь - ",user,"\n Пароль - ",passw)
- print("\nПроверка завершена, пользователь «",user,"» присуствует в Базе Данных.")
 ##Команды нахуй##
 def dbcheck():
  print(moneydb)
  print(users)
 def moneyss():
- if not user == admin:
+ if not user == "Администратор":
   print(moneydb)
  else:
   print("В разработке")
 def helps():
- print("Говно залупа")
+ print("/addmoney ,/helps,/getdb")
 def addmoneys():
  while True:
   try:
-   addmoney = int(input("\nДобавьте сумму на ваш баланс: "))
+   if don == "Администратор":
+    addmoneyus = input("Кому дать? ")
+    addmoney = int(input("\nДобавьте сумму на баланс: "))
+    print(addmoneyus)
+   else:
+    print("К сожалению доступ закрыт Администратором")
   except ValueError:
    print("Ошибка ввода, пожалуйста используйте цифры!")
    print("\nПовторная попытка: ")
    continue
   finally:
    break
-   kek = addmoney + money
-   moneydb[user] = kek
-   users[user] = passw
+   ##WARNING НЕ РАБОТАЕТ##
+   money = money+addmoney
+   adm = addmoneys,money
+   moneydb.update([adm])
 commands = {'/helps': helps,'/addmoney': addmoneys,'/mymoney': moneyss,'/getbd': dbcheck}
 while True:
  comsend = input("Введите команду:")
@@ -69,5 +89,5 @@ while True:
  if command in commands:
   commands[comsend]()
  else:
-  print("Хуйню ввел")
+  print("Не найдено....")
   continue
